a8fb1e22aa313143f43f2b9446d71485e4ecd2b8
Marcus Fritzsch <m@fritschy.de>
2009-11-20 10:34:38 +0100

    importing Control.Monad.Fix instead of Data.Function for hugs and ghc compatibility

--- Fibonacci.hs
+++ Fibonacci.hs
@@ -1,4 +1,4 @@
 module Fibonacci where
-import Data.Function
+import Control.Monad.Fix -- works with hugs and ghc (hugs doesn't know Data.Function)
 fibs0 = fix ((0:) . (scanl (+) 1))
 fibs  = tail $ fibs0
