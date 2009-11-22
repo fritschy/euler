b9dcd0f51294377bd0bb73582ea289558d2f08bc
Marcus Fritzsch <m@fritschy.de>
2009-11-22 21:12:33 +0100

    Problem no. 27 solved

--- Euler027.hs
+++ Euler027.hs
@@ -1,5 +1,6 @@
 module Euler027 where
 import PrimeNumbers
-euler027 = maximum . map (length . snd) . filter ((> 0) . length . snd) $ map primes candidates
-           where candidates = [(a,b) | a <- [-999..999], b <- [-999..999]]
-                 primes = \(a,b) -> ((a,b), takeWhile isPrime [n*n+a*n+b | n <- [0..]])
+euler027 = foldl1 (\a b -> if (l a) < (l b) then b else a) $ map primes candidates
+           where candidates = [(a,b) | a <- [-999..999], b <- [-999..999]] -- odd b?
+                 primes (a,b) = (((a, b), a*b), takeWhile isPrime [n*n+a*n+b | n <- [0..]])
+                 l = length . snd
