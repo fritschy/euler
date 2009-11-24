ceb4cd03caa821eee1516dd310e03c4cef4b33b6
Marcus Fritzsch <m@fritschy.de>
2009-11-24 01:13:20 +0100

    Problem 22 solved, needed sorting of the names too

--- Euler022.hs
+++ Euler022.hs
@@ -2,11 +2,12 @@ module Euler022 where
 
 import Data.Char
 import Utility
+import Data.List
 
 euler022 = do names' <- readFile "data/names.txt"
-              let names = zip [1..] $ map (init . tail) $ splitNames names'
+              let names = zip [1..] . sort . map (init . tail) $ splitNames names'
               putNum . sum $ map val names
-              where val (i, n) = i * (sum $ map (\x -> (ord x) - (1 + ord 'A')) n)
+              where val (i, n) = i * (sum $ map (\x -> 1 + (ord x) - (ord 'A')) n)
                     splitNames [] = []
                     splitNames xs = name : splitNames (drop (1 + length name) xs)
                                     where name = takeWhile (/= ',') xs
