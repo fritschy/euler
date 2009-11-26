147eb5ea011121666336cd30faccd76e3666e21e
Marcus Fritzsch <m@fritschy.de>
2009-11-26 17:25:49 +0100

    Using set in problem solution no. 23 - not solved though

--- Euler023.hs
+++ Euler023.hs
@@ -1,6 +1,8 @@
 module Euler023 where
 import Utility
-euler023 = notSolved -- . sum $ filter findSum [1..28123-1]
-           where abundants = filter ((== Abundant) . classify) numbers
-                 numbers   = [x | x <- [2..28123-1]]
-                 findSum r = null [a | a <- abundants, (r - a) `elem` abundants]
+import Data.Set hiding (filter, null)
+euler023 = notSolved --putStr . show $ filter findSum [1..20161]
+           where abundants' = takeWhile (<= 20162) $ filter ((== Abundant) . classify) numbers
+                 abundants  = fromList abundants'
+                 numbers    = [x | x <- [2..20161]]
+                 findSum r  = null [a | a <- abundants', (r - a) `member` abundants]
