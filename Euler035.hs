576874aa18ac42ff667937fe6569938c79066c26
Marcus Fritzsch <m@fritschy.de>
2009-11-26 16:13:25 +0100

    Added solution to problem no. 35

---
+++ Euler035.hs
@@ -0,0 +1,12 @@
+module Euler035 where
+import PrimeNumbers
+import Utility
+import Data.Char
+euler035 = putNum . length . filter cprime . filter noEven $ takeWhile (< 1000000) primes
+           where noEven n = n < 10 || (null . filter (even . digitToInt) $ show n)
+
+cprime n = and [isPrime . read $ rot sn x | x <- [0..length sn]]
+           where sn = show n
+
+rot [] _ = []
+rot (x:xs) n = if n == 0 then x:xs else rot (xs ++ [x]) (n-1)
