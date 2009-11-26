7549c3d5a3c57b383dd3c87864ad3e803da79052
Marcus Fritzsch <m@fritschy.de>
2009-11-27 00:01:00 +0100

    Added solution to problem no. 41

---
+++ Euler041.hs
@@ -0,0 +1,11 @@
+module Euler041 where
+import PrimeNumbers
+import Utility
+import Data.List
+-- Only 4 and 7 digit pandigital numbers can be prime:
+-- filter snd $ map (\x->(x,(sum [1..x])`mod`3/=0)) [1..9]
+-- [(1,True),(4,True),(7,True)]
+-- all other are always divisible by 3!
+euler041 = putNum . head $ filter isPrime [n | n <- [7654321,7654319..4231], isPandigital n]
+isPandigital n = [1..length d] == d
+                 where d = sort $ digitsOfNumber n
