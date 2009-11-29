173c2ddd9b598c9a7fe7ac4e214d61ad9fd80919
Marcus Fritzsch <m@fritschy.de>
2009-11-29 13:30:19 +0100

    Solution to problem no. 23 seems to be correct, see comment

--- Euler023.hs
+++ Euler023.hs
@@ -1,7 +1,11 @@
 module Euler023 where
 import Utility
 import Data.Set hiding (filter, null)
-euler023 = putStr . show $ filter findSum [1..20161]
-abundants' = takeWhile (<= 20162) $ filter ((== Abundant) . classify) [1..20161]
+-- actually the smallest number from which on every number can be
+-- formed by adding to abundant number is 20162, which is documented
+-- on wikipedia.Nevertheless I am using the hiugher bound to check
+-- my program for correctness.
+euler023 = putNum . sum $ filter findSum [1..28123]
+abundants' = takeWhile (< 28123) $ filter ((== Abundant) . classify) [1..28123]
 abundants  = fromList abundants'
 findSum r  = null [a | a <- abundants', (r - a) `member` abundants]
