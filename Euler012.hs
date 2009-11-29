8e3f19d01904bc3dcb4ec5fcac94c65ae70a48cb
Marcus Fritzsch <m@fritschy.de>
2009-11-29 14:54:55 +0100

    Solved problem no. 12

--- Euler012.hs
+++ Euler012.hs
@@ -1,3 +1,3 @@
 module Euler012 where
 import Utility
-euler012 = putNum . fst . head . filter ((> 500) . length . snd) . zip [1..] $ map (divisors . sumUpTo) [1..]
+euler012 = putNum . fst . head . filter ((> 500) . length . snd) . zip (map sumUpTo [1..]) $ map (divisors . sumUpTo) [1..]
