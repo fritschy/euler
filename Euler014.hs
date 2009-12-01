3d33618c028e2d5194f93c1d1f1da2e29f612ad9
Marcus Fritzsch <m@fritschy.de>
2009-12-01 01:56:12 +0100

    Euler014 uses foldl' (strict foldl) to avoid stack overflow

--- Euler014.hs
+++ Euler014.hs
@@ -1,8 +1,5 @@
 module Euler014 where
 import Data.List
 import Utility
-euler014 = putNum . fst $ foldl1 (\a b -> if (snd a) > (snd b) then a else b) [(x,length $ collatz x) | x <- [1..999999]]
-           where collatz n
-                   | n == 1    = [n]
-                   | otherwise = n : collatz (if even n then div n 2 else 3 * n + 1)
-                
+euler014 = putNum . fst $ foldl' (\a b -> if (snd a) > (snd b) then a else b) (0, 0) [(x,length $ collatz x) | x <- [1..999999]]
+           where collatz = takeWhile (/= 1) . iterate (\n->if even n then div n 2 else 3 * n + 1)
