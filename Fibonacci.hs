763a12452892510767b763982151bf0c53b3d9d9
Marcus Fritzsch <m@fritschy.de>
2011-09-06 15:32:00 +0200

    Newer, faster fibs :: Integral a => [a]

--- Fibonacci.hs
+++ Fibonacci.hs
@@ -6,12 +6,16 @@ module Fibonacci(
 
 import Control.Monad.Fix -- works with hugs and ghc (hugs doesn't know Data.Function)
 
-fibs0, fibs :: Integral a => [a]
-fibs0 = fix ((0:) . (scanl (+) 1))
-fibs  = tail $ fibs0
+fibs_old0, fibs_old :: Integral a => [a]
+fibs_old0 = fix ((0:) . (scanl (+) 1))
+fibs_old  = tail $ fibs0
 
 -- need to flip (++) to et the right ordering (i.e. (+) is commutative, (++) isn't)
 --      (++)   Fn-1 + Fn-2
 -- flip (++)   Fn-2 + Fn-1
 fibWords :: [a] -> [a] -> [[a]]
 fibWords a b = fix ((a:) . (scanl (flip (++)) b))
+
+fibs, fibs0 :: Integral a => [a]
+fibs = f 0 1 where f a b = b:f b (a + b)
+fibs0 = 0:fibs
