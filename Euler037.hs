72720f3526b72ab781a79ee77ceb941a1f7ab32a
Marcus Fritzsch <m@fritschy.de>
2011-09-10 20:12:22 +0200

    Minor improvement for solution to problem no. 37

--- Euler037.hs
+++ Euler037.hs
@@ -6,7 +6,7 @@ euler037 = putNum . sum . take 11 $ candidates
 
 candidates = filter (all isPrime . truncated) . drop 4 $ primes
 
-truncated n = filter (/= 0) $ n : a ++ b
+truncated n = filter (/= 0) $ a ++ b
               where (a,b) = unzip [n `divMod` (10^x) | x<-[1..(l10 n)]]
 
 l10 = round . logBase 10 . fromIntegral
