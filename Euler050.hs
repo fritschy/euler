345c5750b3046bb7dcf5e06b4cb2e72c6a1db518
Marcus Fritzsch <m@fritschy.de>
2011-09-19 00:49:29 +0200

    Minor simplification for Euler050

--- Euler050.hs
+++ Euler050.hs
@@ -5,6 +5,7 @@ import PrimeNumbers
 import Data.List
 
 -- as usual, magic numbers are trial-and-error
-euler050 = putNum . fst . fst . head . sortBy (\((_,x),_) ((_,y),_)->compare y x) . filter (isPrime . fst . fst) $ filter ((<1000000) . fst . fst) [prms d t | t<-[500..700], d<-[0..10]]
+euler050 = putNum . dfst . head . sortBy (\((_,x),_) ((_,y),_)->compare y x) . filter (isPrime . dfst) $ filter ((<1000000) . dfst) [prms d t | t<-[500..700], d<-[0..10]]
 
+dfst = fst . fst
 prms d t = ((sum p, length p), p) where p = take t $ drop d primes
