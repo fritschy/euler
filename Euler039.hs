dd2e50be7532ee5052aa26bc849ce6fa73a3c9d4
Marcus Fritzsch <m@fritschy.de>
2011-09-18 16:45:57 +0200

    Added solution to problem no. 39

---
+++ Euler039.hs
@@ -0,0 +1,6 @@
+module Euler039 where
+
+import Utility
+import Data.List
+
+euler039 = putNum . snd . head . sortBy (\(x,_) (y,_)->compare y x) . map (\(x,y)->(length y, x)) $ filter (not .null . snd) [(d, [(x,y,d-x-y)|x<-[3..d-2], y<-[4,6..x-1], (d-x-y)^2==x^2+y^2]) | d<-[24,28..1000]]
