fa2304e62305a077bdbfdece26c405eb413e23d0
Marcus Fritzsch <m@fritschy.de>
2011-09-27 13:22:20 +0200

    Tweaked solution to problem no. 62

--- Euler062.hs
+++ Euler062.hs
@@ -2,4 +2,10 @@ module Euler062 where
 import Utility
 import Data.List
 import qualified Data.Map as M
-euler062 = putNum . head . head . map (\(_,a)->sort a) . filter ((==5) . length . snd) . M.toList $ foldl' (\acc (k,v) -> M.insertWith (++) k v acc) M.empty (map ((\x->(sort $ show x, [x])) . (^3)) [1000..9999])
+euler062 = putNum
+         . head
+         . map (minimum . snd)
+         . filter ((==5) . length . snd)
+         . M.toList
+         . foldl' (\acc (k,v) -> M.insertWith (++) k v acc) M.empty
+         $ map ((\x->(sort $ show x, [x])) . (^3)) [1000..9999]
