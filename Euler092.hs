e3f4d9e23658fc2f438314d58c841c3819d8d663
Marcus Fritzsch <m@fritschy.de>
2012-08-06 16:38:56 +0200

    092: slightly cleaner and faster

--- Euler092.hs
+++ Euler092.hs
@@ -1,8 +1,6 @@
 module Euler092 where
 import Utility
-euler092 = putNum . length $ filter (\x->89 == l x) [1..(10000000-1)]
-
-l :: Int -> Int
-l n = if n == 89 || n == 1
-        then n
-        else l (sum . map (^2) $ digitsOfNumber n)
+euler092 = putNum . length $ filter l [1..(10000000-1)]
+  where l 89 = True
+        l  1 = False
+        l  n = l (sum . map (^2) $ digitsOfNumber n)
