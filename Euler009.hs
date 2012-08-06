4e62b121719462e591a1891441bdde345f1b472a
Marcus Fritzsch <m@fritschy.de>
2012-08-06 17:05:19 +0200

    009: slightly different logic

--- Euler009.hs
+++ Euler009.hs
@@ -1,5 +1,5 @@
 module Euler009 where
 import Utility
-euler009 = putNum $ product abc
-           where abc = head $ [[x, y, z] | x <- [3,6..1000], y <- [4,8..1000], z <- [5,10..1000], cond x y z]
-                 cond x y z = x*x + y*y == z*z && x + y + z == 1000
+euler009 = putNum abc
+           where abc = head $ [x * y * z | x <- [3,6..1000], y <- [4,8..1000], z <- [5,10..1000], cond x y z]
+                 cond x y z = x + y + z == 1000 && x*x + y*y == z*z
