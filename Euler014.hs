974e7bbae9d3da30718c0c0852044d0e9bfe8ed7
Marcus Fritzsch <m@fritschy.de>
2012-08-06 17:05:45 +0200

    014: no lists, 10-fold performance increase

--- Euler014.hs
+++ Euler014.hs
@@ -1,7 +1,10 @@
 module Euler014 where
 import Data.List
+import Data.Bits
 import Utility
 euler014 = putNum
          . fst
-         $ foldl' (\a b -> if (snd a) > (snd b) then a else b) (0, 0) [(x,length $ collatz x) | x <- [1..999999]]
-           where collatz = takeWhile (/= 1) . iterate (\n->if even n then div n 2 else 3 * n + 1)
+         $ foldl' (\a b -> if (snd a) > (snd b) then a else b) (0, 0) [(x, collatz x) | x <- [1..999999] :: [Int]]
+           where collatz n = c' n 0
+                 c' 1 c    = c
+                 c' n c    = c' (if n .&. 1 == 0 then shiftR n 1 else 3 * n + 1) (c+1)
